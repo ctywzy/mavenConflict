3b9318d74296e485b017279ac0db4823486d3373
wangzy <zuyu.wzy@alibaba-inc.com>
2020-08-17 14:27:04 +0800

    add: function perfect

--- moduleFive/src/main/java/moduleFiveStartApplication.java
+++ moduleFive/src/main/java/moduleFiveStartApplication.java
@@ -1,13 +1,14 @@
 import com.sun.org.apache.xpath.internal.FoundIndex;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class moduleFiveStartApplication {
 
     @Test
     public void mainMethod(){
         FourMethod fourMethod = new FourMethod();
         ThreeMethod threeMethod = new ThreeMethod();
-
-
     }
 }
